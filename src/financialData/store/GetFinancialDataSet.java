ab075728147f73c01d111463ee05091f06045e18
James <James@DESKTOP-2D6R8T2.Home>
2018-07-23 20:38:05 +0100

    Still being built

---
+++ src/financialData/store/GetFinancialDataSet.java
@@ -0,0 +1,5 @@
+package financialData.store;
+
+public class GetFinancialDataSet {
+
+}
---
+++ src/financialDataKeys/RecordKeys.java
@@ -0,0 +1,24 @@
+package financialDataKeys;
+
+public final class RecordKeys {
+	public static final String DATE = "open";
+	public static final String TIME = "open";
+	
+	public static final String OPEN = "open";
+	public static final String HIGH = "open";
+	public static final String LOW = "open";
+	public static final String CLOSE = "open";
+	
+	public static final String VOLUME = "open";
+	
+	public static final String THREEMOVAVG = "open";
+	public static final String FIVEMOVAVG = "open";
+	public static final String TENMOVAVG = "open";
+	public static final String TWENTYMOVAVG = "open";
+	public static final String FIFTYMOVAVG = "open";
+	public static final String ONEHUNMOVAVG = "open";
+	public static final String TWOHUNMOVAVG = "open";
+	
+	
+	
+}
