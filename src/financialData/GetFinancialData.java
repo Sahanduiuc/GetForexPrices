fd6cc7358b543856e70f5f4892eecc81e76e37f4
James <James@DESKTOP-2D6R8T2.Home>
2018-07-23 17:52:12 +0100

    Simplified way methods check if a different dataset is needed

--- src/financialData/GetFinancialData.java
+++ src/financialData/GetFinancialData.java
@@ -6,109 +6,75 @@ public class GetFinancialData {
 	private static String currentDataSetKey;
 	
 	public static BigDecimal getFinancialRecord(String asset, String timeFrame, String attribute, int timePeriod) {
-		String key = asset + "_" + timeFrame;
-		
-		if( !currentDataSetKey.equals(key) ) {
-			changeCurrentDataset();
-		}
+		checkIfDatasetNeedsChanging(asset, timeFrame);
 		
 		return null;
 	}
 	
 	public static BigDecimal getFinancialRecord(String asset, String timeFrame, String attribute, String date) {
-		String key = asset + "_" + timeFrame;
-		
-		if( !currentDataSetKey.equals(key) ) {
-			changeCurrentDataset();
-		}
+		checkIfDatasetNeedsChanging(asset, timeFrame);
 		
 		return null;
 	}
 	
 	public static BigDecimal[] getFinancialRecords(String asset, String timeFrame, 
 								String attribute, int startPeriod, int endPeriod) {
-		String key = asset + "_" + timeFrame;
-		
-		if( !currentDataSetKey.equals(key) ) {
-			changeCurrentDataset();
-		}
+		checkIfDatasetNeedsChanging(asset, timeFrame);
 		
 		return null;
 	}
 	
 	public static BigDecimal[] getFinancialRecords(String asset, String timeFrame, 
 								String attribute, String startDate, String endDate) {
-		String key = asset + "_" + timeFrame;
-		
-		if( !currentDataSetKey.equals(key) ) {
-			changeCurrentDataset();
-		}
+		checkIfDatasetNeedsChanging(asset, timeFrame);
 		
 		return null;
 	}
 	
 	public static String getDate(String asset, String timeFrame, String attribute, int timePeriod) {
-		String key = asset + "_" + timeFrame;
-		
-		if( !currentDataSetKey.equals(key) ) {
-			changeCurrentDataset();
-		}
+		checkIfDatasetNeedsChanging(asset, timeFrame);
 		
 		return null;
 	}
 	
 	public static BigDecimal getTime(String asset, String timeFrame, String date) {
-		String key = asset + "_" + timeFrame;
-		
-		if( !currentDataSetKey.equals(key) ) {
-			changeCurrentDataset();
-		}
+		checkIfDatasetNeedsChanging(asset, timeFrame);
 		
 		return null;
 	}
 	public static BigDecimal[] getTimes(String asset, String timeFrame, String date) {
-		String key = asset + "_" + timeFrame;
-		
-		if( !currentDataSetKey.equals(key) ) {
-			changeCurrentDataset();
-		}
+		checkIfDatasetNeedsChanging(asset, timeFrame);
 		
 		return null;
 	}
 	
 	public static BigDecimal[] getTimes(String asset, String timeFrame, int timePeriod) {
-		String key = asset + "_" + timeFrame;
-		
-		if( !currentDataSetKey.equals(key) ) {
-			changeCurrentDataset();
-		}
+		checkIfDatasetNeedsChanging(asset, timeFrame);
 		
 		return null;
 	}
 	
 	public static BigDecimal[] getDates(String asset, String timeFrame, 
 										int startPeriod, int endPeriod) {
-		String key = asset + "_" + timeFrame;
-		
-		if( !currentDataSetKey.equals(key) ) {
-			changeCurrentDataset();
-		}
+		checkIfDatasetNeedsChanging(asset, timeFrame);
 		
 		return null;
 	}
 	
 	public static BigDecimal[] getAllDates(
 					String asset, String timeFrame) {
+		checkIfDatasetNeedsChanging(asset, timeFrame);
+		
+		return null;
+	}
+	
+	private static void checkIfDatasetNeedsChanging(String asset, String timeFrame) {
 		String key = asset + "_" + timeFrame;
 		
 		if( !currentDataSetKey.equals(key) ) {
 			changeCurrentDataset();
 		}
-		
-		return null;
 	}
-	
-	
 	
 	private static void changeCurrentDataset() {
 		
